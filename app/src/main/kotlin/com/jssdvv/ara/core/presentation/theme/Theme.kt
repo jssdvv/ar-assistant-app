e9a7e363e18e4ccdf1542a4929fe7e18b751c780
jssdvv <jssdvv@gmail.com>
2024-12-27 00:32:27 -0500

    Add immersive UI with transparent system bars

--- app/src/main/kotlin/com/jssdvv/ara/core/presentation/theme/Theme.kt
+++ app/src/main/kotlin/com/jssdvv/ara/core/presentation/theme/Theme.kt
@@ -7,6 +7,7 @@ import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
@@ -90,7 +91,7 @@ private val LightColorScheme = lightColorScheme(
 )
 
 @Composable
-fun AraTheme(
+fun Theme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit
 ) {
@@ -102,13 +103,14 @@ fun AraTheme(
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
+            window.statusBarColor = Color.Transparent.toArgb()
+            window.navigationBarColor = Color.Transparent.toArgb()
             WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
         }
     }
     MaterialTheme(
         colorScheme = colorScheme,
-        typography = AraTypography,
+        typography = Typography,
         content = content
     )
 }
