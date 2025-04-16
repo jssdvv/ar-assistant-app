6be2e5d0b55c648c898e4d14f3be8571bdc53e85
jssdvv <jssdvv@gmail.com>
2025-04-16 01:54:27 -0500

    Add source directories routing structure per feature

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.gradle.internal.declarativedsl.parsing.main
+
 plugins {
     alias(libs.plugins.jetbrains.kotlin.plugin.serialization)
     alias(libs.plugins.jetbrains.compose.compiler)
@@ -13,9 +15,22 @@ android {
     compileSdk = libs.versions.android.compileSdk.get().toInt()
 
     sourceSets.all {
+        res.srcDirs(
+            "src/main/res",
+            "src/main/res/ara",
+            "src/main/res/core",
+            "src/main/res/scanner",
+            "src/main/res/machines",
+            "src/main/res/machines/machines",
+            "src/main/res/machines/machine-details",
+            "src/main/res/machines/activities",
+            "src/main/res/machines/activity-details",
+            "src/main/res/machines/markers",
+            "src/main/res/machines/ar-session",
+            "src/main/res/inventory"
+        )
         java.srcDir("src/main/kotlin")
         manifest.srcFile("src/main/AndroidManifest.xml")
-        res.srcDirs("src/main/res")
     }
     defaultConfig {
         applicationId = "com.jssdvv.ara"
@@ -39,11 +54,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "21"
     }
     buildFeatures {
         compose = true
@@ -60,7 +75,6 @@ android {
 }
 
 dependencies {
-    implementation(libs.androidx.material3.adaptive.navigation.suite.android)
     // Testing
     testImplementation(libs.junit)
     androidTestImplementation(platform(libs.androidx.compose.bom))
@@ -71,6 +85,7 @@ dependencies {
     // Android
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.material3.adaptive.navigation.suite.android)
 
     // Kotlin
     implementation(libs.jetbrains.kotlinx.serialization.json)
@@ -91,7 +106,10 @@ dependencies {
 
     // Camera
     implementation(libs.bundles.androidx.camera)
+
+    // Barcodes
     implementation(libs.google.mlkit.barcodeScanning)
+    implementation(libs.google.zxing.core)
 
     // Augmented Reality
     implementation(libs.sceneview.arsceneview)
@@ -102,14 +120,17 @@ dependencies {
     kapt(libs.androidx.room.compiler)
     implementation(libs.androidx.room.ktx)
 
+    // Datastore
+    implementation(libs.androidx.datastore.preferences)
+
     // Dependency Injection
     implementation(libs.google.dagger.hilt.android)
     kapt(libs.google.dagger.hilt.android.compiler)
     implementation(libs.androidx.hilt.navigation.compose)
 
+    // Images Loader
     implementation(libs.coil.compose)
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-guava:1.9.0")
-
 }
 
 kapt {
