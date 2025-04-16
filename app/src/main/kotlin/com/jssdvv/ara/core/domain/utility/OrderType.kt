173527a58e63e6c674f2cf7fd68c760b0828e8e1
jssdvv <jssdvv@gmail.com>
2025-04-16 01:46:58 -0500

    Add variants of ordering

--- app/src/main/kotlin/com/jssdvv/ara/core/domain/utility/OrderType.kt
+++ app/src/main/kotlin/com/jssdvv/ara/core/domain/utility/OrderType.kt
@@ -1,5 +1,28 @@
 package com.jssdvv.ara.core.domain.utility
 
-enum class OrderType {
-    ASCENDING, DESCENDING
+import androidx.annotation.StringRes
+import com.jssdvv.ara.R
+
+/**
+ * Enum representing the different types of ordering available for a list of items.
+ *
+ * @property [orderTypeNameId] The name of the order type.
+ * @property [orderTypeAlphabetNameId] The name of the order type for alphabetical ordering.
+ * @property [orderTypeDateNameId] The name of the order type for date ordering.
+ */
+enum class OrderType(
+    @StringRes val orderTypeNameId: Int,
+    @StringRes val orderTypeAlphabetNameId: Int,
+    @StringRes val orderTypeDateNameId: Int,
+) {
+    ASCENDING(
+        orderTypeNameId = R.string.order_type_ascending_label,
+        orderTypeAlphabetNameId = R.string.order_type_ascending_alphabet_label,
+        orderTypeDateNameId = R.string.order_type_ascending_date_label
+    ),
+    DESCENDING(
+        orderTypeNameId = R.string.order_type_descending_label,
+        orderTypeAlphabetNameId = R.string.order_type_descending_alphabet_label,
+        orderTypeDateNameId = R.string.order_type_descending_date_label
+    )
 }
