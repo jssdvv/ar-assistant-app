c6b13e7055cfcb9b66ecda2169ba2d9a82ff4cbe
Jesús <jssdvv@gmail.com>
2026-05-03 23:34:34 -0500

    refactor: delete unused extension

--- app/src/main/kotlin/com/jssdvv/ara/core/domain/utility/Iterable.kt
+++
@@ -1,6 +0,0 @@
-package com.jssdvv.ara.core.domain.utility
-
-inline fun <T, R : Iterable<T>> R.forEachApply(block: T.() -> Unit): R {
-    for (element in this) { element.block() }
-    return this
-}
